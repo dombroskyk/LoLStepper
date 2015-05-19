c562264ee41c0ea20f06217d996b93bfd2ce232a
Dave Korhumel <dpk2442@gmail.com>
2015-05-19 18:16:09 -0400

    add basic routing and template pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    res.render('splash');
+});
+
+router.get('/about', function(req, res) {
+    res.render('about');
+});
+
+router.get('/matches/:summonerName', function(req, res) {
+    res.render('matches', { summonerName: req.param('summonerName') });
+});
+
+router.get('/match/:matchId', function(req, res) {
+    res.render('match', { matchId: req.param('matchId') });
 });
 
 module.exports = router;
