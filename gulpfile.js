9ab45b8e155b35d0b013f301b9cd01156e1b8b4e
kowzrule <dombroskyk@gmail.com>
2015-05-23 04:42:12 -0400

    add mongod command to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var exec = require('child_process').exec;
 
 gulp.task('sass', function () {
     gulp.src('./assets/sass/main.scss')
@@ -11,4 +12,14 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
     gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('mongod', function(){
+	exec('mongod --config ./cache_db/mongodb.conf', function( err, stdout, stderr){
+		console.log(stdout);
+		console.log(stderr);
+		if(err !== null){
+			console.log( 'exec error: ' + err);
+		}
+	});
 });
