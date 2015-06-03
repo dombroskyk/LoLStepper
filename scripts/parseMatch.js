1114fdc47913e71cbfa249c190b721d4e633e243
kowzrule <dombroskyk@gmail.com>
2015-06-03 18:41:44 -0400

    changed some files using tabs to four spaces. add mongodb caching for matches. add fetching of a match.

--- scripts/parseMatch.js
+++ scripts/parseMatch.js
@@ -1,26 +1,55 @@
 var https = require('https');
 var config = require('../config');
+var mongoclient = require('mongodb').MongoClient;
 
-function parseMatch( matchId, renderCallback ) {
-	//check if match is already in Mongo
-	//if yes, fetch
-	//if no, query for it, store in mongo
-	
-	var match = {};
-	var matchReqOptions = {
-		host: config.RIOT_API.API_HOST,
-		path: config.RIOT_API.MATCH_PATH + matchId + '?includeTimeline=true&api_key=' + config.RIOT_API.API_KEY
-	}
-	https.get(matchReqOptions, function(res) {
-		//res.setEncoding("utf8"); i dont think the encoding is utf8
-		res.on('data', function(chunk) {
-			//parse JSON, store match
-		});
-		res.on('end', function() {
-			//parse match for desired information
-			renderCallback(null, match);
-		});
-	});
+function parseMatch(matchId, renderCallback) {
+    //check if match is already in Mongo
+    //if yes, fetch
+    var mongoUrl = 'mongodb://localhost:27017/lolstepper';
+    mongoclient.connect(mongoUrl, function(err, db) {
+        if(err){
+            console.log('error: ' + err);    
+        }
+        var collection = db.collection('matches');
+        collection.findOne({_id: parseInt(matchId)}, function(findErr, doc) {
+            if(findErr){
+                console.log('find err: ' + findErr);
+            }else if(doc){
+                //match found, render
+                renderCallback(null, doc); 
+                db.close();
+            }else{
+                //no match cached, fetch, store, and render
+                var matchReqOptions = {
+                    host: config.RIOT_API.API_HOST,
+                    path: config.RIOT_API.MATCH_PATH + matchId + '?includeTimeline=true&api_key=' + config.RIOT_API.API_KEY
+                };
+                //fetch
+                var matchData = "";
+                var matchJson;
+                https.get(matchReqOptions, function(matchRes) {
+                    matchRes.setEncoding('utf8');
+                    matchRes.on('data', function(matchChunk) {
+                        //aggregate data
+                        matchData += matchChunk;
+                    });
+                    matchRes.on('end', function() {
+                        //parse match for desired information
+                        matchJson = JSON.parse(matchData); 
+                        matchJson._id = matchJson.matchId;
+                        collection.insert(matchJson, function(insertErr, record) {
+                            if(insertErr){
+                                console.log("insert error: " + insertErr);
+                            }
+                            //render
+                            renderCallback(null, matchJson);
+                            db.close();
+                        });
+                    });
+                });
+            }
+        });
+    });
 }
 
 module.exports = parseMatch;
--- scripts/parseMatchHistory.js
+++ scripts/parseMatchHistory.js
@@ -2,97 +2,97 @@ var https = require('https');
 var config = require('../config');
 
 function parseMatchHistory(summonerName, renderCallback) {
-	var summonerReqOptions = {
-		host: config.RIOT_API.API_HOST,
-		path: config.RIOT_API.SUMMONER_PATH + encodeURIComponent(summonerName) + '?api_key=' + config.RIOT_API.API_KEY
-	}
-	var summonerData = "";
-	var summonerId;
-	https.get(summonerReqOptions, function(summonerRes) {
-		//TODO: handle error codes, custom error pages?
-		if(summonerRes.statusCode != 200){
-			renderCallback(summonerRes.statusCode, {});
-			return;
-		}
-		summonerRes.setEncoding('utf8');
-		summonerRes.on('data', function(summonerChunk) {
-			//gather data from response parts
-			summonerData += summonerChunk;
-		});
-		summonerRes.on('end', function() {
-			//parse JSON, get summoner id
-			if(summonerRes != ""){
-				summonerData = JSON.parse(summonerData);
-			}else{
-				renderCallback(null, {});
-			}
-			summonerId = summonerData[summonerName.toLowerCase().replace(/ /g,'')].id;
+    var summonerReqOptions = {
+        host: config.RIOT_API.API_HOST,
+        path: config.RIOT_API.SUMMONER_PATH + encodeURIComponent(summonerName) + '?api_key=' + config.RIOT_API.API_KEY
+    }
+    var summonerData = "";
+    var summonerId;
+    https.get(summonerReqOptions, function(summonerRes) {
+        //TODO: handle error codes, custom error pages?
+        if(summonerRes.statusCode != 200){
+            renderCallback(summonerRes.statusCode, {});
+            return;
+        }
+        summonerRes.setEncoding('utf8');
+        summonerRes.on('data', function(summonerChunk) {
+            //gather data from response parts
+            summonerData += summonerChunk;
+        });
+        summonerRes.on('end', function() {
+            //parse JSON, get summoner id
+            if(summonerRes != ""){
+                summonerData = JSON.parse(summonerData);
+            }else{
+                renderCallback(null, {});
+            }
+            summonerId = summonerData[summonerName.toLowerCase().replace(/ /g,'')].id;
 
-			var matchHistoryReqOptions = {
-				host: config.RIOT_API.API_HOST,
-				path: config.RIOT_API.MH_PATH + summonerId + '?beginIndex=0&api_key=' + config.RIOT_API.API_KEY
-			}
-			var matchesData = "";
-			var matchesJSON;
-			https.get(matchHistoryReqOptions, function(mhRes) {
-				if(mhRes.statusCode != 200){
-					renderCallback(mhRes.statusCode, {});
-					return;
-				}
-				//parse match history json
-				mhRes.setEncoding('utf8');
-				mhRes.on('data', function(mhChunk) {
-					matchesData += mhChunk;
-				});
-				mhRes.on('end', function() {
-					matchesJSON = JSON.parse(matchesData);
-					//pull out the information that we want
-					var reducedJSON = [];
-					for(var i = 0; i < matchesJSON.matches.length; i++){
-						var curr = matchesJSON.matches[i];
-						var newJSON = {
-							mapId: curr.mapId,
-							queueType: curr.queueType,
-							matchDuration: curr.matchDuration,
-							matchType: curr.matchType,
-							matchId: curr.matchId,
-							items: {
-								item0: curr.participants[0].stats.item0,
-								item1: curr.participants[0].stats.item1,
-								item2: curr.participants[0].stats.item2,
-								item3: curr.participants[0].stats.item3,
-								item4: curr.participants[0].stats.item4,
-								item5: curr.participants[0].stats.item5,
-								item6: curr.participants[0].stats.item6
-							},
-							stats: {
-								winner: curr.participants[0].stats.winner,
-								largestMultiKill: curr.participants[0].stats.largestMultiKill,
-								kills: curr.participants[0].stats.kills,
-								deaths: curr.participants[0].stats.deaths,
-								assists: curr.participants[0].stats.assists,
-								minionsKilled: curr.participants[0].stats.minionsKilled,
-								goldEarned: curr.participants[0].stats.goldEarned
-							},
-							spell1Id: curr.participants[0].spell1Id,
-							spell2Id: curr.participants[0].spell2Id,
-							championId: curr.participants[0].championId,
-							matchMode: curr.matchMode,
-						};
+            var matchHistoryReqOptions = {
+                host: config.RIOT_API.API_HOST,
+                path: config.RIOT_API.MH_PATH + summonerId + '?beginIndex=0&api_key=' + config.RIOT_API.API_KEY
+            }
+            var matchesData = "";
+            var matchesJSON;
+            https.get(matchHistoryReqOptions, function(mhRes) {
+                if(mhRes.statusCode != 200){
+                    renderCallback(mhRes.statusCode, {});
+                    return;
+                }
+                //parse match history json
+                mhRes.setEncoding('utf8');
+                mhRes.on('data', function(mhChunk) {
+                    matchesData += mhChunk;
+                });
+                mhRes.on('end', function() {
+                    matchesJSON = JSON.parse(matchesData);
+                    //pull out the information that we want
+                    var reducedJSON = [];
+                    for(var i = 0; i < matchesJSON.matches.length; i++){
+                        var curr = matchesJSON.matches[i];
+                        var newJSON = {
+                            mapId: curr.mapId,
+                            queueType: curr.queueType,
+                            matchDuration: curr.matchDuration,
+                            matchType: curr.matchType,
+                            matchId: curr.matchId,
+                            items: {
+                                item0: curr.participants[0].stats.item0,
+                                item1: curr.participants[0].stats.item1,
+                                item2: curr.participants[0].stats.item2,
+                                item3: curr.participants[0].stats.item3,
+                                item4: curr.participants[0].stats.item4,
+                                item5: curr.participants[0].stats.item5,
+                                item6: curr.participants[0].stats.item6
+                            },
+                            stats: {
+                                winner: curr.participants[0].stats.winner,
+                                largestMultiKill: curr.participants[0].stats.largestMultiKill,
+                                kills: curr.participants[0].stats.kills,
+                                deaths: curr.participants[0].stats.deaths,
+                                assists: curr.participants[0].stats.assists,
+                                minionsKilled: curr.participants[0].stats.minionsKilled,
+                                goldEarned: curr.participants[0].stats.goldEarned
+                            },
+                            spell1Id: curr.participants[0].spell1Id,
+                            spell2Id: curr.participants[0].spell2Id,
+                            championId: curr.participants[0].championId,
+                            matchMode: curr.matchMode,
+                        };
 
-						reducedJSON.push(newJSON);
-					}
-					renderCallback(null, reducedJSON);
-				});
-			}).on('error', function(e) {
-				//implement file logging
-				console.log('Problem with match history request: ' + e.message);
-			});
-		});
-	}).on('error', function(e) {
-		//implement file logging
-		console.log('Problem with summoner name request: ' + e.message);
-	});
+                        reducedJSON.push(newJSON);
+                    }
+                    renderCallback(null, reducedJSON);
+                });
+            }).on('error', function(e) {
+                //implement file logging
+                console.log('Problem with match history request: ' + e.message);
+            });
+        });
+    }).on('error', function(e) {
+        //implement file logging
+        console.log('Problem with summoner name request: ' + e.message);
+    });
 }
 
 module.exports = parseMatchHistory;
