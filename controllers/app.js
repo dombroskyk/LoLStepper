dec322413b6defac63845b9ac8e76eadaaa275b5
Dave Korhumel <dpk2442@gmail.com>
2015-06-04 21:37:37 -0700

    remove use strict statements

--- controllers/app.js
+++ controllers/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = {
 
     home: function (req, res) {
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
