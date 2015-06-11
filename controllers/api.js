690556580d17265daa6f8d5f228e7a1a093ec6cb
kowzrule <dombroskyk@gmail.com>
2015-06-10 20:54:39 -0400

    add api controller. add partial view for ajax call

---
+++ controllers/api.js
@@ -0,0 +1,38 @@
+module.exports = {
+    matchHistory: function (req, res) {
+        var parseMatchHistory = require('../includes/parseMatchHistory'),
+            summonerName = req.query.summonerName;
+        parseMatchHistory(summonerName, matchHistoryDataRender);
+        function matchHistoryDataRender(err, matches) {
+            //TODO: handle error codes
+            if(!err){
+                //OK
+                res.render('matchesData', { summonerName: summonerName, matchHistory: matches });
+            }else if(err == 400){
+                //Bad Request
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else if(err == 401){
+                //Unauthorized
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else if(err == 404){
+                //Game not found
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else if(err == 422){
+                //Summoner hasn't played since start of 2013
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else if(err == 429){
+                //Rate limit exceeded
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else if(err == 500){
+                //Internal server error
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else if(err == 503){
+                //Service unavailable
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }else{
+                //Should never be reached
+                res.render('matchesData', { summonerName: summonerName, matchHistory: {} });
+            }
+        }
+    }
+};
--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 var express = require('express'),
     router = express.Router(),
-    appController = require('./controllers/app');
+    appController = require('./controllers/app'),
+    apiController = require('./controllers/api');
 
 router.get('/', appController.home);
 router.get('/about', appController.about);
 router.get('/matches/?', appController.matchHistory);
 router.get('/match/?', appController.match);
+router.get('/api/matches/?', apiController.matchHistory);
 
 module.exports = router;
